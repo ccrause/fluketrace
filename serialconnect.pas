ff06c3dae32862ee82bcc725e539d49d578e528b
ccrause <christo.crause@gmail.com>
2020-03-20 21:31:32 +0200

    Added functionality to automatically list available serial ports.

---
+++ serialconnect.pas
@@ -0,0 +1,46 @@
+unit serialconnect;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm2 }
+
+  TForm2 = class(TForm)
+    ConnectButton: TButton;
+    CancelButton: TButton;
+    ComboBox1: TComboBox;
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+uses
+  serialutils;
+
+{$R *.lfm}
+
+{ TForm2 }
+
+procedure TForm2.FormShow(Sender: TObject);
+begin
+  ComboBox1.Items.Delimiter := ',';
+  ComboBox1.Items.Text := GetSerialPortNames;
+  if ComboBox1.Items.Count > 0 then
+    ComboBox1.ItemIndex := 0;
+end;
+
+end.
+
--- serialutils.pas
+++ serialutils.pas
@@ -56,6 +56,9 @@ type
     property SerialHandle: THandle read FHandle;
   end;
 
+  // Return list of serial ports as a comma separated list
+  function GetSerialPortNames: string;
+
 const
   TCGETS2 = $802C542A; //longint(2150388778);
   TCSETS2 = $402C542B; //1076646955;
@@ -68,6 +71,29 @@ uses
   {$ELSE}
   windows;
   {$ENDIF}
+
+  { Translated from include/linux/serial.h }
+  type
+    TSerialStruct = packed record
+      typ: Integer;
+      line: Integer;
+      port: Cardinal;
+      irq: Integer;
+      flags: Integer;
+      xmit_fifo_size: Integer;
+      custom_divisor: Integer;
+      baud_base: Integer;
+      close_delay: Word;
+      io_type: Char;
+      reserved_char: Char;
+      hub6: Integer;
+      closing_wait: Word; // time to wait before closing
+      closing_wait2: Word; // no longer used...
+      iomem_base: ^Char;
+      iomem_reg_shift: Word;
+      port_high: Cardinal;
+      iomap_base: LongWord; // cookie passed into ioremap
+    end;
 
 { TSerialObj }
 
@@ -287,6 +313,98 @@ begin
 
   inherited Destroy;
 end;
+
+////////////////////////////////////////////////////////////////////////
+  {$IFDEF MSWINDOWS}
+  function GetSerialPortNames: string;
+  var
+    reg: TRegistry;
+    l, v: TStringList;
+    n: integer;
+  begin
+    l := TStringList.Create;
+    v := TStringList.Create;
+    reg := TRegistry.Create;
+    try
+  {$IFNDEF VER100}
+  {$IFNDEF VER120}
+      reg.Access := KEY_READ;
+  {$ENDIF}
+  {$ENDIF}
+      reg.RootKey := HKEY_LOCAL_MACHINE;
+      reg.OpenKey('\HARDWARE\DEVICEMAP\SERIALCOMM', false);
+      reg.GetValueNames(l);
+      for n := 0 to l.Count - 1 do
+        v.Add(reg.ReadString(l[n]));
+      Result := v.CommaText;
+    finally
+      reg.Free;
+      l.Free;
+      v.Free;
+    end;
+  end;
+  {$ENDIF}
+  {$IFNDEF MSWINDOWS}
+  // Idea adapted from PascalSCADA project's serialport.pas file:
+  // http://pascalscada.svn.sourceforge.net/viewvc/pascalscada/trunk/serialport.pas?revision=702&view=markup
+  // More prefixes exist see e.g. :
+  // http://comments.gmane.org/gmane.comp.ide.lazarus.general/46750
+
+  {$IFDEF UNIX}
+  var
+    {$IFDEF LINUX}
+    PortPrefix: array[0..1] of string = ({'ttyS',} 'ttyUSB', 'ttyACM');  // Possibly several other names too
+    {$ENDIF}
+    {$IFDEF FREEBSD}
+    PortPrefix:array[0..2] of string = ('cuad', 'cuau', 'ttyu'); // http://www.freebsd.org/doc/handbook/serial.html
+    {$ENDIF}
+    {$IFDEF NETBSD}
+    PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
+    {$ENDIF}
+    {$IFDEF OPENBSD}
+    PortPrefix:array[0..0] of string = ('cuad');  // Don't have better info at the moment
+    {$ENDIF}
+  {$ENDIF}
+
+  function GetSerialPortNames: string;
+  var
+    i: Integer;
+    sr : TSearchRec;
+    f: TSerialHandle;
+    val: integer;
+    ser: TSerialStruct;
+  begin
+    Result := '';
+    try
+      for i := 0 to high(PortPrefix) do
+        if FindFirst('/dev/' + PortPrefix[i] + '*', LongInt($FFFFFFFF), sr) = 0 then  // from patch
+        begin
+          repeat
+            if (sr.Attr and $FFFFFFFF) = Sr.Attr then
+            begin
+              begin
+                f := FpOpen('/dev/'+sr.Name, O_RDWR or O_NOCTTY or O_NONBLOCK);
+                if f <> 0 then
+                begin
+                  val := FpIOCtl(f, TIOCGSERIAL, @ser);
+                  if (val <> -1) then
+                    Result := Result + '/dev/' + sr.Name + ',';
+                  SerClose(f);
+                end;
+              end;
+            end;
+          until FindNext(sr) <> 0;
+        end;
+    finally
+      FindClose(sr);
+      // remove trailing comma
+      i := length(Result);
+      if (i > 0) and (Result[i] = ',') then
+        Result[i] := #0;
+    end;
+  end;
+  {$ENDIF}
+////////////////////////////////////////////////////////////
 
 end.
 
